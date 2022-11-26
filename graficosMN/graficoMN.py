f7bce58c72e37f662e9da498a658b865b19911a7
merilimv <murilovpinheiro@gmail.com>
2022-11-25 21:43:43 -0300

    CSV arrumado, POO reestruturado e Gráficos Corrigidos

--- graficosMN/graficoMN.py
+++ graficosMN/graficoMN.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
+
+#######################################################
+#CASO ENTRE NESSE CÓDIGO LEIA O README.MD DESSE ARQUIVO
+#######################################################
 
 def createLineIT(cor, label, metodo, dataframe, ax):
     ticks = [0.001, 0.01, 0.05, 0.1]
@@ -23,11 +26,11 @@ def plotIteracoes():
         createLineIT(cores[i], nomes[i], metodos[i], dataframe, ax)
     ax.set_xlabel('Epsilon')
     ax.set_ylabel('Número de Iterações')
-    ax.set_title('Número de Iterações para os Diferentes Métodos')
+    ax.set_title('Número de Iterações para os Diferentes no Benchmark')
     ax.set_xticks(x, ticks)
     ax.legend()
     ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
-    plt.show()
+    fig.savefig("./graficosMN/Iteracao.png")
 
 def createLineER(cor, label, metodo, dataframe, ax):
     ticks = [0.001, 0.01, 0.05, 0.1]
@@ -35,12 +38,13 @@ def createLineER(cor, label, metodo, dataframe, ax):
     y = dataframe.loc[dataframe['metodo'] == metodo].sort_values(by = ['epsilon'])
     y = y['erromedio']
     ax.plot(x, y, marker = 'o', color = cor, label = label)
+    
 
 def plotErro():
     fig, ax = plt.subplots(figsize = (10,8))
     dataframe = pd.read_csv("./graficosMN/metodos.csv")
     dataframe = dataframe[['epsilon', 'erromedio', 'metodo']]
-    ticks = [0.001, 0.01, 0.05, 0.1] # Poderia fazer isso dinamicamente mas como é fixo vou manter
+    ticks = [0.001, 0.01, 0.05, 0.1]
     x = list(range(len(ticks)))
     metodos = ('GaussSeidelInverso', 'GaussSeidelNormal', 'GaussJacobiInverso', 'GaussJacobiNormal')
     nomes = ("Gauss Seidel por Inversa", "Gauss Seidel", "Gauss Jacobi por Inversa", "Gauss Jacobi")
@@ -49,13 +53,15 @@ def plotErro():
         createLineER(cores[i], nomes[i], metodos[i], dataframe, ax)
     ax.set_xlabel('Epsilon')
     ax.set_ylabel('Valor de Erro Médio')
-    ax.set_title('Erro Médio de Diferentes Métodos')
+    ax.set_title('Erro Médio de Diferentes Métodos no Benchmark')
     ax.set_xticks(x, ticks)
     ax.legend()
     ax.grid(which='both', color='grey', linewidth=1, linestyle='-', alpha=0.2)
-    plt.show()
+    fig.savefig("./graficosMN/Erro.png")
 
 
 plotIteracoes()
 plotErro()
 #cheguei num resultado próximo ao desejado.
+
+#CASO ENTRE NESSE CÓDIGO LEIA O READM.MD DESSE ARQUIVO
